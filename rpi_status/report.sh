f2d14f455e827a9c5cdd0794581da910166136af
hqiu <9558181@qq.com>
2019-09-05 14:28:02 +0800

    统一使用printf，免得某些得不到的项变成null

--- rpi_status/report.sh
+++ rpi_status/report.sh
@@ -29,10 +29,10 @@ echo "FREE: "$MEMF
 MODEL=`cat /proc/device-tree/model`
 echo "MODEL:"$MODEL
 echo "====================================="
-JSON=`jo -p host="$HOST" ip="$ETH0IP" ipv6="$ETH0IP6" wip="$WLANIP" wipv6="$WLANIP6" cpu="$CPUNAME" core="$CPUCORE" arch="$CPUARCH" temperature="$CPUTEMP" freq="$CPUFREQ" model="$MODEL" mem="$MEMT" memf="$MEMF" usage="$CPUUSAGE" scriptver="$SCRIPTVER"`
-if [ $? -ne 0 ]; then 
-echo "jo not installed"
-JSON=`printf '{"%s":"%s","%s":"%s","%s":"%s","%s":"%s","%s":"%s","%s":"%s","%s":"%s","%s":"%s","%s":"%s","%s":%s,"%s":%s,"%s":%s,"%s":%s,"%s":%s,"%s":%s}' "host" "$HOST" "ip" "$ETH0IP" "ipv6" "$ETH0IP6" "wip" "$WLANIP" "wipv6" "$WLANIP6" "cpu" "$CPUNAME" "arch" "$CPUARCH" "temperature" "$CPUTEMP" "model" "$MODEL" "usage" "$CPUUSAGE" "core" "$CPUCORE" "mem" "$MEMT" "memf" "$MEMF" "freq" "0" "scriptver" "$SCRIPTVER"`
-fi
+#JSON=`jo -p host="$HOST" ip="$ETH0IP" ipv6="$ETH0IP6" wip="$WLANIP" wipv6="$WLANIP6" cpu="$CPUNAME" core="$CPUCORE" arch="$CPUARCH" #temperature="$CPUTEMP" freq="$CPUFREQ" model="$MODEL" mem="$MEMT" memf="$MEMF" usage="$CPUUSAGE" scriptver="$SCRIPTVER"`
+#if [ $? -ne 0 ]; then 
+#echo "jo not installed"
+JSON=`printf '{"%s":"%s","%s":"%s","%s":"%s","%s":"%s","%s":"%s","%s":"%s","%s":"%s","%s":"%s","%s":"%s","%s":%s,"%s":%s,"%s":%s,"%s":%s,"%s":%s,"%s":%s}' "host" "$HOST" "ip" "$ETH0IP" "ipv6" "$ETH0IP6" "wip" "$WLANIP" "wipv6" "$WLANIP6" "cpu" "$CPUNAME" "arch" "$CPUARCH" "temperature" "$CPUTEMP" "model" "$MODEL" "usage" "$CPUUSAGE" "core" "$CPUCORE" "mem" "$MEMT" "memf" "$MEMF" "freq" "$CPUFREQ" "scriptver" "$SCRIPTVER"`
+#fi
 echo $JSON
 curl -X POST --data "`echo -n $JSON | sed 's/"/\\"/g; s/\\r//g'`" http://test.qiuhao.online:8081/rpi_status/
